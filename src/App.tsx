ae1872f78d866f68ad8eb6286ca42abb4da30b89
MerlinAlex <merlinalex@qq.com>
2023-03-03 15:09:15 +0800

    feat: router and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { useRoutes } from 'react-router-dom'
+import { useRoutes, Link } from 'react-router-dom'
 import routes from './router'
 
 function App() {
   return (
     <div className="App">
-      Hello React + TypeScript!
-      {useRoutes(routes)}
+      <div className='nav'>
+        <Link to='/discover'>发现音乐</Link>
+        <Link to='/mine'>我的音乐</Link>
+        <Link to='/focus'>关注</Link>
+        <Link to='/download'>下载客户端</Link>
+      </div>
+      { useRoutes(routes) }
     </div>
   )
 }
